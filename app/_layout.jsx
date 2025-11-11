962a3d8a029511b0ed84efcfe7002ebd2b33ef8c
aurelahasanaj <162487885+aurelahasanaj@users.noreply.github.com>
2025-11-12 00:41:10 +0100

    Update _layout.jsx

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,47 +1,27 @@
-import { Stack, useRouter } from "expo-router";
-import { useEffect, useState } from "react";
-import { getAuth, onAuthStateChanged } from "firebase/auth";
-import { app } from "../firebaseConfig";
-import { ActivityIndicator, View } from "react-native";
+
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 
 export default function RootLayout() {
-  const [checkingAuth, setCheckingAuth] = useState(true);
-  const [user, setUser] = useState(null);
-  const router = useRouter();
-  const auth = getAuth(app);
-
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (u) => {
-      setUser(u);
-      setCheckingAuth(false);
-
-      if (!u) {
-        console.log("👤 S'ka përdorues → po kalon në login...");
-        router.replace("/login");
-      } else {
-        console.log("✅ Përdorues i kyçur:", u.email);
-      }
-    });
-    return unsubscribe;
-  }, []);
-
-  if (checkingAuth) {
-    return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <ActivityIndicator size="large" color="#4A90E2" />
-      </View>
-    );
-  }
-
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="index" />
-      <Stack.Screen name="login" />
-      <Stack.Screen name="addItem" />
-      <Stack.Screen name="foundItem" />
-      <Stack.Screen name="profile" />
-      <Stack.Screen name="about" />
-    </Stack>
+    <>
+      <StatusBar style="dark" />
+      <Stack 
+        screenOptions={{ 
+          headerShown: false,
+          
+          animation: 'slide_from_right',
+          gestureEnabled: true,
+          gestureDirection: 'horizontal',
+        }}
+      >
+        <Stack.Screen name="index" />
+        <Stack.Screen name="addItem" />
+        <Stack.Screen name="foundItem" />
+        <Stack.Screen name="profile" />
+        <Stack.Screen name="about" />
+      </Stack>
+    </>
   );
 }
 
