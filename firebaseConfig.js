cd4547daa12bc65136730b51b6480916a8ebf4e4
aurelahasanaj <162487885+aurelahasanaj@users.noreply.github.com>
2025-11-11 13:39:57 +0100

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,1 +1,11 @@
-
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyDd3FLMfTnwdfDNJxILtx7l0YqUFnKyIx0",
+  authDomain: "refind-bonus.firebaseapp.com",
+  projectId: "refind-bonus",
+  storageBucket: "refind-bonus.firebasestorage.app",
+  messagingSenderId: "525836493151",
+  appId: "1:525836493151:web:b70f4566f0ebabf693d6ea",
+  measurementId: "G-RHMZZHQCXP"
+};
+export const app = initializeApp(firebaseConfig);
